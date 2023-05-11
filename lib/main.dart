8e74849ef9c880c813504231615c590ab1fae575
deploytg <d4n73@cyberdude.com>
2023-05-11 23:23:46 +0200

    init

---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Croassant',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Croassant'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  List<Widget> croassantList = [];
+
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body: GestureDetector(
+        onTapDown: (details){
+          setState(() {
+            croassantList.add(Croassant(details.globalPosition));
+           }); 
+        },
+        child : croassantList.isEmpty 
+          ? Center(child:Text("Tap for Croassant")) :
+          Stack(
+            children: croassantList
+          ),
+       ), 
+    );
+  }
+}
+
+
+
+class Croassant extends StatelessWidget {
+  var offset;
+  Croassant(this.offset);
+  @override
+
+  Widget build(BuildContext context) {
+
+    return Positioned(
+      top: offset.dy, 
+      left:offset.dx, 
+      child: Transform.rotate(
+        angle: Random().nextDouble() * (180 - 0) + 0 , 
+        child: Image.asset("assets/croassant.png"),
+      ), 
+    );
+
+  }
+
+}
